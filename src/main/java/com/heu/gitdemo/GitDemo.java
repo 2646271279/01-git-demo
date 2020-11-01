29f65c948a621c8f88dd4c711af2222cbfaf5e17
chenbinglobal <chenbinglobal@123.com>
2020-11-01 11:56:30 +0800

    second commit

--- src/main/java/com/heu/gitdemo/GitDemo.java
+++ src/main/java/com/heu/gitdemo/GitDemo.java
@@ -3,5 +3,6 @@ package com.heu.gitdemo;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("IDEA整合git");
+        System.out.println("我对文件进行了修改");
     }
 }
